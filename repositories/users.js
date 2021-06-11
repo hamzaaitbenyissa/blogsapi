d43ddbb5ca26511274f10e1ea32bd9d7e5b8a0ff
Hamza AIT BEN YISSA <hamzaaitbenyissa@gmail.com>
2021-06-11 02:05:43 +0100

    Update Routes

--- repositories/users.js
+++ repositories/users.js
@@ -1,122 +1,58 @@
 var { User } = require('../models')
 
-// exportation
 module.exports = {
-	getAllUsers: function(){
+	getAllUsers: ()=>{
 		return User.findAll();
-	},
-	getUsers: function(offset=0, limit=10){
-		return User.findAll({offset: offset, limit: limit});
-	},
-	getUserByRole: function(role){
-		return User.findAll({
-			where: {
-				role: role
-			}
-		});
-	},
-	getAdmins: function(){
-		return this.getUserByRole('admin')
-	},
-	getAuthors: function() {
-		return this.getUserByRole('author')
-	},
-	getGuests: function(){
-		return this.getUserByRole('guest')
-	},
-	getUser: function(id){
-		return User.findOne({
-			where: {
-				id: id
-			}
-		})
-	},
-	getUserByEmail: function(email){
-		return User.findOne({
-			where: {
-				email: email
-			}
-		});
-	},
-	addUser: async function(userDate){
-		// TODO: before adding the user, check wheter an user with the same email already exists
-		// if yes so refuse the request and send error message
-		// otherwise add the user
-		var user = await User.create(userDate)
-		return user
-	},
-	updateUser: async function(id, userData){
-		return await User.update(userData, {
-			where: {
-				id: id
-			}
-		})
-	},
-	deleteUser: async function(id){
-		return await User.destroy({
-			where: {
-				id: id
-			}
-		})
-	},
+},
+
+    getUsers: (offset = 0, limit = 10)=> {
+    return User.findAll({
+    limit :limit,
+    offset: offset,
+    });
+    },
+
+    getAdmins: ()=>{
+    return User.findAll({
+    where : {role : "admin"}    
+    });
+     },
+    
+    getAuthors:()=>{
+        return User.findAll({
+        where : {role : "author"}    
+        });
+         },    
+    
+    getGuests: ()=>{
+        return User.findAll({
+        where : {role : "guest"}    
+        });
+         },
+    
+    getUser: (id)=>{        
+    return User.findOne({
+    where: {
+    id: id
+    }})},
+
+    getUserByEmail: (email)=> {
+     return User.findOne({
+      where : {email:email}  
+     })   
+     },
+
+    addUser : (newuser)=> {
+    User.create(newuser)
+     },
+
+    updateUser: (id)=>{  
+    User.update({where: {id:id}})
+     },
+
+    deleteUser: (id)=>{
+        user.destroy({where: { id: id }})
+     },
+    // D'autres méthodes jugées utiles
 
 }
-
-
-
-// const { User } = require('../models' );
-// const user = require('../models/user');
-
-// module.exports = {
-//     getAllUsers: ()=>{
-//     return User.findAll()
-//     },
-
-//     getUsers:(offset = 0, limit = 10)=> {
-//     return User.findAll({
-//     limit :limit,
-//     offset: offset,
-//     });
-//     },
-
-//     getAdmins: ()=>{
-//     return User.findAll({
-//     where : {role : "admin"}    
-//     });
-//      },
-
-//     getAuthors:()=>{
-//         return User.findAll({
-//         where : {role : "author"}    
-//         });
-//          },    
-    
-//     getGuests: ()=>{
-//         return User.findAll({
-//         where : {role : "guest"}    
-//         });
-//          },
-    
-//     getUser: (id)=>{ 
-//      return User.findBypk(id)  
-//     },
-
-//     getUserByEmail: (email)=> {
-//      return User.findOne({
-//       where : {email:email}  
-//      })   
-//      },
-
-//     addUser(newuser) {
-//     User.create(newuser)
-//      },
-
-//     updateUser: (id)=>{  
-//     User.update({where: {id:id}})
-//      },
-
-//     deleteUser: (id)=>{
-//         user.destroy({where: { id: id }})
-//      },
-//     // D'autres méthodes jugées utiles
-//     }
--- routes/users.js
+++ routes/users.js
@@ -4,34 +4,40 @@ const usersRepo = require('../repositories/users')
 /* GET users listing. */
 router.get('/', async function(req, res, next){
 res.send(await usersRepo.getAllUsers());
-
-// return usersRepo.getAllUsers();
 });
 
+router.get('/admins', async function(req, res, next) {
+  res.send(await usersRepo.getAdmins())
+  });
+
 // /* GET users with pagination. */
-// // router.get('/:limit' , async function(req, res, next) {
-// //   res.send(await usersRepo.getAllUsers())
-// //   });
-  
-// /* GET user by id. */
-// router.get('/:id', async function(req, res, next) {
-//   res.send(await usersRepo.getUser(id))
+// router.get('/?limit=10&offset=0', async function(req, res, next) {
+//   res.send(await usersRepo.getUsers(req.params.offset,req.params.limit))
 //   });
   
 
-// /* Add New User . */
-// router.post('/users' , async function(req, res, next) {
-//   res.send(await usersRepo.addUser(req.body))
-//   });
+  
+/* GET user by id. */
+router.get('/:id', async function(req, res, next) {
+  res.send(await usersRepo.getUser(req.params.id))
+  });
+  
 
-// /* Update an User . */
-// router.put('/users' , async function(req, res, next) {
-//   res.send(await usersRepo.updateUser(req.body))
-//   });
 
-// /* Delete  an User . */
-// router.delete('/users/:id', async function(req, res, next) {
-//   res.send(await usersRepo.deleteUser(id))
-//   });
+/* Add New User . */
+router.post('/users' , async function(req, res, next) {
+  res.send(await usersRepo.addUser(req.body))
+  });
+
+/* Update an User . */
+router.put('/users' , async function(req, res, next) {
+  res.send(await usersRepo.updateUser(req.body))
+  });
+
+/* Delete  an User . */
+router.delete('/users/:id', async function(req, res, next) {
+  res.send(await usersRepo.deleteUser(id))
+  });
+
 
 module.exports = router;
