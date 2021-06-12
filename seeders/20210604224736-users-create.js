46618a82ac6cd864a2491e042c8385a4ee7532b6
Hamza AIT BEN YISSA <hamzaaitbenyissa@gmail.com>
2021-06-12 14:06:04 +0100

    Create Fake Data for testing our api

--- seeders/20210604224736-users-create.js
+++ seeders/20210604224736-users-create.js
@@ -1,32 +1,34 @@
-const { fake } = require("faker");
 const faker=require("faker")
 'use strict';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+
+    // const data=[];
+    // const roles=["admin","guest","author"];
+    // for(let i=0;i<20;i++){
+    // data.push({
+    //   username: faker.name.findName(),
+    //   email : faker.internet.email(),
+    //   password: faker.internet.password(),
+    //   role : roles[Math.floor(Math.random() * (3)) + 0],     
+		// 	createdAt: new Date(),
+		// 	updatedAt: new Date()
+    // })
+    // }
     
-    const data=[];
-    const roles=["admin","guest","author"];
-    for(let i=0;i<20;i++){
-    data.push({
-      username: faker.name.findName(),
-      email : faker.internet.email(),
-      password: faker.internet.password(),
-      role : roles[Math.floor(Math.random() * (3)) + 0],     
-			createdAt: new Date(),
-			updatedAt: new Date()
-    })
-    }
-    await queryInterface.bulkInsert('Users', data, {});
+    // await queryInterface.bulkInsert('Users', data, {});
     
   },
 
+
+
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
   }
 };
